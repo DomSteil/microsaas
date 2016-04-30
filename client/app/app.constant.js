b637fee63c0d051b50ae1c4dd11640c52672c816
Nilesh Shah <nileshshah@Nileshs-MacBook-Pro.local>
2016-04-30 13:32:53 -0700

    customized to point to BlockApp server on Azure

--- client/app/app.constant.js
+++ client/app/app.constant.js
@@ -3,7 +3,7 @@
 
 angular.module('pizzaSwapApp.constants', [])
 
-.constant('appConfig', {userRoles:['guest','user','admin'],apiEndPoint:'http://strato-dev3.blockapps.net/eth/v1.1/',keyserver:'http://localhost:8000/'})
+.constant('appConfig', {userRoles:['guest','user','admin'],apiEndPoint:'http://52.165.36.32/eth/v1.1/',keyserver:'http://52.165.36.32:8000/'})
 
 ;
 })(angular);
--- server/config/environment/shared.js
+++ server/config/environment/shared.js
@@ -4,6 +4,8 @@ exports = module.exports = {
   // List of user roles
   userRoles: ['guest', 'user', 'admin'],
 
-  apiEndPoint: 'http://strato-dev3.blockapps.net/eth/v1.1/',
-  keyserver: 'http://localhost:8000/'
+  apiEndPoint: 'http://52.165.36.32/eth/v1.1/',
+  //keyserver: 'http://strato-dev3.blockapps.net:8000/'
+  //keyserver: 'http://localhost:8000/'
+  keyserver: 'http://52.165.36.32:8000/'
 };
