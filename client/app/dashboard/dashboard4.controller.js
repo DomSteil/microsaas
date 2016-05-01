e32b3b26a26eb48f3e629c5ec35d881379d32b0f
Dominic Steil <dominicsteil@hotmail.com>
2016-04-30 19:31:59 -0700

    Create dashboard4.controller.js

---
+++ client/app/dashboard/dashboard4.controller.js
@@ -0,0 +1,51 @@
+'use strict';
+
+(function() {
+
+class Dashboard4Controller {
+
+  constructor($http, $scope, $stateParams, appConfig, cfpLoadingBar, localStorageService) {
+    this.$http = $http;
+    this.awesomeThings = [];
+    $scope.accounts = [];
+
+    /**
+     * We are saving user data in local storage.
+     * You can very easily swap out local storage for a real database.
+     */
+    $scope.name = localStorageService.get('user');
+
+    var getAccountData = function(address) {
+      $http.get(appConfig.apiEndPoint + 'account?address=' + address).then(response =>{
+        $scope.balance = (response.data[0].balance / 1000000000000000000).toFixed(3);
+        localStorageService.set('address', address);
+      });
+    };
+
+    $http.get(appConfig.keyserver + 'users/' + $stateParams.name).then(response =>{
+      $scope.user = response.data[0];
+      getAccountData($scope.user);
+    });
+
+
+
+    $http.get(appConfig.keyserver + 'contracts/Contact').then(response => {
+      response.data.forEach(account => {
+        $http.get(appConfig.keyserver + 'contracts/Contact/' + contract + '/state/').then(response => {
+          console.log("MICROSAAScontact");
+          console.log(response);
+          response.data.address = account;
+          response.data.id = account.substr(0, 4);
+          $scope.account.push(response.data);
+        });
+      });
+    });
+
+  }
+
+}
+
+angular.module('microsaas')
+  .controller('DashboardController', DashboardController);
+
+})();
