a4587e95c699e842e5c121e972346d765754a89e
Dominic Steil <dominicsteil@hotmail.com>
2016-04-30 22:32:33 -0700

    Update events3.router.js

--- client/app/swap/events/events3.router.js
+++ client/app/swap/events/events3.router.js
@@ -1,12 +1,12 @@
 'use strict';
 
-angular.module('pizzaSwapApp')
+angular.module('microsaas')
   .config(function($stateProvider) {
     $stateProvider
       .state('events', {
         url: '/swap/{id:string}/events',
-        templateUrl: 'app/swap/events/events3.html',
-        controller: 'EventsController',
+        templateUrl: 'app/swap/events/events.html',
+        controller: 'Events3Controller',
         controllerAs: 'Events'
       });
   });
