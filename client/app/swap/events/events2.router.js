9ab6ba8106989f9f22acab0ccb73c16b002ec71f
Dominic Steil <dominicsteil@hotmail.com>
2016-04-30 23:09:13 -0700

    Update events2.router.js

--- client/app/swap/events/events2.router.js
+++ client/app/swap/events/events2.router.js
@@ -5,7 +5,7 @@ angular.module('microsaas')
     $stateProvider
       .state('events', {
         url: '/swap/{id:string}/events',
-        templateUrl: 'app/swap/events/events.html',
+        templateUrl: 'app/swap/events/events2.html',
         controller: 'Events2Controller',
         controllerAs: 'Events'
       });
