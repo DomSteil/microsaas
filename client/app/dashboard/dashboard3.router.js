e14cad4845ea8c3cb4fdb0021eafef2de77e9e45
Dominic Steil <dominicsteil@hotmail.com>
2016-04-30 23:05:43 -0700

    Update dashboard3.router.js

--- client/app/dashboard/dashboard3.router.js
+++ client/app/dashboard/dashboard3.router.js
@@ -5,7 +5,7 @@ angular.module('microsaas')
     $stateProvider
       .state('dashboard', {
         url: '/dashboard/{name:string}',
-        templateUrl: 'app/dashboard/dashboard.html',
+        templateUrl: 'app/dashboard/dashboard3.html',
         controller: 'Dashboard3Controller',
         controllerAs: 'dashboard'
       });
