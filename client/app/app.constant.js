c18ab10f17e46aee20f952e0eae796f34c235264
Dominic Steil <dominicsteil@hotmail.com>
2016-04-30 19:06:45 -0700

    Update app.constant.js

--- client/app/app.constant.js
+++ client/app/app.constant.js
@@ -1,7 +1,7 @@
 (function(angular, undefined) {
 'use strict';
 
-angular.module('pizzaSwapApp.constants', [])
+angular.module('microsaas.constants', [])
 
 .constant('appConfig', {userRoles:['guest','user','admin'],apiEndPoint:'http://52.165.36.32/eth/v1.1/',keyserver:'http://52.165.36.32:8000/'})
 
